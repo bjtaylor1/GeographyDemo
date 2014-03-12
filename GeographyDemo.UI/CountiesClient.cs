68262058eb5c70f644a95be2c604df48fc23e3a6
ben <bentaylortheonly@gmail.com>
2014-03-12 13:50:15 +0000

    added async behaviour

--- GeographyDemo.UI/CountiesClient.cs
+++ GeographyDemo.UI/CountiesClient.cs
@@ -5,14 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
 using GeographyDemo.Contracts;
+using System.Threading;
 
 namespace GeographyDemo.UI
 {
-    public class CountiesClient : ClientBase<ICountiesService>, ICountyProvider
+    public class CountiesClient : ClientBase<ICountiesService>, ICountyProvider, IAsyncCountyProvider
     {
         public CountyData[] GetCounties()
         {
+            Thread.Sleep(2000);
             return Channel.GetCounties();
+        }
+
+        public Task<CountyData[]> GetCountiesAsync()
+        {
+            return Task.Run(() => GetCounties());
         }
     }
 }
---
+++ GeographyDemo.UI/DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GeographyDemo.UI
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Func<object, bool> canExecute;
+        private Action<object> execute;
+        public DelegateCommand(Func<object, bool> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if(execute != null)
+            {
+                execute(parameter);
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var canExecuteChanged = CanExecuteChanged;
+            //take a reference to avoid threading issues (unsubscribe between checking and calling)
+            if(canExecuteChanged != null)
+            {
+                canExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
